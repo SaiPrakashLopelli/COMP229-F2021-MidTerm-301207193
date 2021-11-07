b0aa8a7aa5d9563e4595acaa88c52fc001491be6
Saiprakash <saiprakashraolopelly@gmail.com>
2021-11-07 08:37:43 +0530

    final commit

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/books229"
+  "URI": "mongodb+srv://saiprakashlopelli:9695949321@mongodatabaseserversmid.3esjy.mongodb.net/books?retryWrites=true&w=majority"
 };
